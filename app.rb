509e980b017340455aedc5bc68e1cebc9cd624fe
Mrooski <107450558+Mrooski@users.noreply.github.com>
2023-10-29 00:46:40 +0000

    Added payment calc functionality and links to new

--- app.rb
+++ app.rb
@@ -24,3 +24,29 @@ get("/square_root/new") do
     erb(:square_root_output)
   end
 end
+
+get("/payment/new") do
+  if params.empty?
+    erb(:payment)
+  elsif
+    @apr = params.fetch("user_apr").to_f
+    @years = params.fetch("user_years").to_f
+    @principal = params.fetch("user_pv").to_f
+
+    months = @years * 12
+    monthly_rate = @apr / 100 / 12
+
+    @numerator = monthly_rate * @principal
+    @denominator = 1 - (1 + monthly_rate) ** (-months)
+
+    @payment = @numerator / @denominator
+    @payment = @payment.to_fs(:currency)
+    @apr = @apr.to_fs(:percentage)
+    @principal = @principal.to_fs(:currency)
+
+    erb(:payment_output)
+  end
+end
+
+get("/random/new") do
+end
